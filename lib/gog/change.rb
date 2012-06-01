48e775acd72529f48fde8903a0dd36bc599d5d05
Thomas Darde <thomas@rougecardinal.fr>
2012-06-01 08:57:21 +0200

    Using a constant for commit's change extracting.

--- lib/gog/change.rb
+++ lib/gog/change.rb
@@ -2,39 +2,39 @@ class Gog
   class Change
 
     attr_accessor :header, :message, :commit
-    
+
     def initialize(header, message, commit)
       @header = header
       @message = message
       @commit = commit
-    end    
-    
+    end
+
     def closest_tag
       @closest_tag ||= find_closest_tag
     end
-    
+
     def to_s
       "#{@header}: #{message}"
     end
-    
+
     private
-    
+
     def find_closest_tag
       available_tags = Gog::Log.repo_tags
       return if available_tags.empty?
-      
+
       available_tags_by_sha = {}
       available_tags.each do |tag|
         available_tags_by_sha[tag.commit.sha] = tag
       end
-      
+
       Change.find_tag_in_self_or_parents(self, available_tags_by_sha)
     end
-    
+
     def self.find_tag_in_self_or_parents(change, tags)
       tag = tags[change.commit.sha]
-      tag || find_tag_in_parent(change.parents.first, tags)
+      tag || find_tag_in_self_or_parents(change.parents.first, tags)
     end
-    
+
   end
 end
--- lib/gog/commit.rb
+++ lib/gog/commit.rb
@@ -1,14 +1,18 @@
 require 'gog/change'
 
+ONE_LINE_ONE_WORD_REGEX = /^\%([^\s]+)\s(.*)$/
+
 class Gog
   module Commit
+
     def self.extract_changes(commit)
       message = commit.message
-      matched = message.scan(/^\%([^\s]+)\s(.*)$/)
+      matched = message.scan(ONE_LINE_ONE_WORD_REGEX)
       return [] if matched.empty?
       matched.map do |match|
         Gog::Change.new match[0], match[1], commit
       end
     end
+
   end
 end
