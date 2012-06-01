e482ab1defa0d00987b4b3693729a9e26754ea6d
Thomas Darde <thomas@rougecardinal.fr>
2012-06-01 08:57:43 +0200

    Log is a module %Enhancemnet log is a module.

--- lib/gog/log.rb
+++ lib/gog/log.rb
@@ -1,46 +1,49 @@
 require 'grit'
 
 class Gog
-  class Log
+  module Log
     include Grit
 
     @@repo
-    
-    def self.init(repo = Repo.new("."))
-      @@repo = repo
-      @@commit_changes = @@repo.commits.map do |commit|
-        Gog::Commit.extract_changes commit
+
+    class << self
+
+      def init(repo = Repo.new("."))
+        @@repo = repo
+        @@changes = @@repo.commits.map do |commit|
+          Gog::Commit.extract_changes commit
+        end
+        @@changes.select! { |change| !change.empty? }
+        @@changes.flatten!
+        self
       end
-      @@commit_changes.select! { |change| !change.empty? }
-      @@commit_changes.flatten!
-      self
-    end
-    
-    def self.changes
-      @@commit_changes
-    end
-    
-    def self.changes_by_tag
-      tags = Hash.new()
-      @@commit_changes.each do |change|
-        tag_name = change.closest_tag.name
-        tags[tag_name] = [] unless tags[tag_name]
-        tags[tag_name] << change
+
+      def changes
+        @@changes
       end
-      tags
-    end
-    
-    def self.to_s
-      if @@commit_changes.empty?
-        'No changes found. Read https://github.com/goglog/gog for instructions.'
-      else
-        @@commit_changes.join("\n")
+
+      def changes_by_tag
+        tags = Hash.new()
+        @@changes.each do |change|
+          tag_name = change.closest_tag.name
+          tags[tag_name] = [] unless tags[tag_name]
+          tags[tag_name] << change
+        end
+        tags
       end
+
+      def to_s
+        if @@changes.empty?
+          'No changes found. Read https://github.com/goglog/gog for instructions.'
+        else
+          @@changes.join("\n")
+        end
+      end
+
+      def repo_tags
+        @@tags ||= @@repo.tags.sort {|a,b| a.name <=> b.name }
+      end
+
     end
-    
-    def self.repo_tags
-      @@tags ||= @@repo.tags
-    end
-    
   end
 end
