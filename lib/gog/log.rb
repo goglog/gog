234e1f20a6d6e1894c3de3d652fc2f5561e2cb6d
Thomas Darde <thomas@rougecardinal.fr>
2012-06-08 14:18:36 +0200

    Pretty printing

--- lib/gog/log.rb
+++ lib/gog/log.rb
@@ -9,9 +9,9 @@ class Gog
     class << self
 
 
-      def init(repo = Grit::Repo.new("."))
+      def init(repo = Grit::Repo.new('.'))
         @@repo = repo
-        @@changes = @@repo.commits.map do |commit|
+        @@changes = @@repo.commits('master', 30).map do |commit|
           Gog::Commit.extract_changes commit
         end
         @@changes.select! { |change| !change.empty? }
@@ -24,13 +24,15 @@ class Gog
       end
 
       def changes_by_tag
-        reversed_tags = Hash.new
+        reversed_tags = Hash.new {|h,k| h[k] = [] }
         tags = Hash.new
+        # Grouping changes by parent tags (impossible in the other way with Grit)
         @@changes.each do |change|
           tag_name = change.closest_parent_tag_name
-          reversed_tags[tag_name] = [] unless reversed_tags[tag_name]
           reversed_tags[tag_name] << change
         end
+
+        # Reversing tag array
         tags_array = reversed_tags.to_a
         tags_array.each_with_index do |tag_and_changes, index|
           tag = tag_and_changes[0]
